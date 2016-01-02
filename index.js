71772418a572ae5e3ef0eddec65aefacc3a5772d
Carl Woodward <carl@88cartell.com>
2016-01-02 18:29:07 +1100

    feat: running code blocks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var exec = require('child_process').exec;
 var showdown = require('showdown');
 var cheerio = require('cheerio');
 var rand = require('generate-key');
@@ -39,7 +40,7 @@ function buildFilename(filepath, elem, $) {
 
 //
 // Returns a list of code blocks that look like:
-// [{filename: '...', content: '...'}, ...]
+// [{filename: '...', content: '...', language: '...'}, ...]
 //
 function extractCodeBlocks(filepath) {
   return new Promise(function(fulfull, reject) {
@@ -55,7 +56,8 @@ function extractCodeBlocks(filepath) {
           var language = $(elem).attr('class').split(' ')[0];
           return {
             filename: buildFilename(filepath, elem, $),
-            content: $(elem).text()
+            content: $(elem).text(),
+            language: language
           };
         });
         fulfull(codeBlocks);
@@ -64,6 +66,84 @@ function extractCodeBlocks(filepath) {
   });
 }
 
+//
+// Returns the same list of code blocks
+// as what is passed in, but each filename includes
+// the directory where it is stored.
+//
+function writeCodeBlocks(codeBlocks) {
+  return new Promise(function(fulfull, reject) {
+    var dir = './.runnable-markdown-' + rand.generateKey(7);
+    fs.mkdir(dir, function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        codeBlocks.forEach(function(codeBlock, index) {
+          var filepath = dir + '/' + codeBlock.filename;
+          fs.writeFile(filepath, codeBlock.content, function(err) {
+            if (err) {
+              console.err(err);
+              return reject(err);
+            } else {
+              codeBlock.filename = filepath;
+              // TODO: convert to a promise for codeblock and promise.all.
+              if (index === codeBlocks.length - 1) {
+                fulfull(codeBlocks);
+              }
+            }
+          });
+        });
+      }
+    });
+  });
+}
+
+function runner(codeBlock) {
+  if (codeBlock.filename.indexOf('package.json') !== -1) {
+    return 'npm install';
+  } else if (codeBlock.filename.indexOf('Gemfile') !== -1) {
+    return 'bundle install';
+  } else if (codeBlock.language === 'javascript') {
+    return 'node';
+  } else {
+    return codeBlock.language;
+  }
+}
+
+//
+// Runs all code blocks sequentially.
+// Uses specially runners for files like Gemfile and package.json.
+// Also cleans up directory.
+// Returns codeBlocks.
+//
+function runCodeBlocks(codeBlocks) {
+  if (codeBlocks.length === 0) {
+    return Promise.resolve(codeBlocks);
+  }
+  return new Promise(function(fulfull, reject) {
+    var dir = path.dirname(codeBlocks[0].filename);
+    codeBlocks.forEach(function(codeBlock, index) {
+      var filenameWithoutDir = codeBlock.filename.replace(dir + '/', '');
+      var command = runner(codeBlock) + ' ' + filenameWithoutDir;
+      exec(command, {cwd: dir}, function(error, stdout, stderr) {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(stdout);
+          console.error(stderr);
+
+          // TODO: convert to a promise for codeblock and promise.all.
+          if (index === codeBlocks.length - 1) {
+            fulfull(codeBlocks);
+          }
+        }
+      });
+    });
+  });
+}
+
 module.exports = {
-  extractCodeBlocks: extractCodeBlocks
+  extractCodeBlocks: extractCodeBlocks,
+  writeCodeBlocks: writeCodeBlocks,
+  runCodeBlocks: runCodeBlocks
 };
--- test/test-main.js
+++ test/test-main.js
@@ -13,3 +13,38 @@ describe('#extractCodeBlocks()', function() {
       });
   });
 });
+
+describe('#writeCodeBlocks()', function() {
+  it('writes a directory with all files', function(done) {
+    main.writeCodeBlocks([{
+      filename: 'hello.js',
+      content: 'console.log("test");',
+      language: 'javascript'
+    }])
+    .then(function(codeBlocks) {
+      assert.equal(codeBlocks.length, 1);
+      done();
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
+});
+
+describe('#runCodeBlocks()', function() {
+  it('run a list of code blocks', function(done) {
+    main.writeCodeBlocks([{
+      filename: 'hello.js',
+      content: 'console.log("test");',
+      language: 'javascript'
+    }])
+    .then(main.runCodeBlocks)
+    .then(function(codeBlocks) {
+      assert.equal(codeBlocks.length, 1);
+      done();
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
+});
