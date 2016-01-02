74303f86df336ebae942d60430a64c426548b6f0
Carl Woodward <carl@88cartell.com>
2016-01-02 16:45:16 +1100

    feat: support more filetypes

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ var cheerio = require('cheerio');
 var rand = require('generate-key');
 
 function fileExtensionForLanguage(language) {
-  switch(language) {
-    case 'javascript':
-      return '.js';
-    case 'ruby':
-      return '.rb';
-    case 'json':
-      return '.json';
-  }
+  return {
+    javascript: '.js',
+    ruby: '.rb',
+    json: '.json',
+    python: '.py',
+    coffeescript: '.coffee',
+    go: '.go',
+    erlang: '.erl',
+    perl: '.pl',
+    rust: '.rs'
+  }[language] || '.unknown';
 }
 
 function buildFilename(filepath, elem, $) {
@@ -43,6 +46,7 @@ function run(filepath) {
         var html = converter.makeHtml(text);
         var $ = cheerio.load(html);
         var codeBlocks = $('pre code').map(function(index, elem) {
+          var language = $(elem).attr('class').split(' ')[0];
           return {
             filename: buildFilename(filepath, elem, $),
             content: $(elem).text()
